c21cd69262a3ab581076a073b35617065b6f6f13
jsy7787@gmail.com <jsy7787@gmail.com@761075a2-e22b-11de-8065-53a27c5290e0>
2009-12-13 13:03:13 +0000

    GLUT 기본 화면 보이는 것 까지..

--- trunk/Freetype2 Ex/GLUT/JglutCommon.cpp
+++ trunk/Freetype2 Ex/GLUT/JglutCommon.cpp
@@ -17,6 +17,7 @@ void _JinitGLUT()
 	glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
 }
 
+
 //////////////////////////////////////////////////////////////////////////
 // Function Name: JinitGLUT()
 // In: GLUT_METHOD Pointer
@@ -25,8 +26,21 @@ void _JinitGLUT()
 //		이 함수는 GLUT를 초기화 하는 함수 이다.
 //		GLUT 콜백 함수에서 필요로 하는 사용자 지정 함수를
 //		구조체 포인터에 담아서 인자로 넘겨 받는다.
-void JinitGLUT( GLUT_METHOD* pMethod, int argc, char** argv )
+void JinitGLUT( JGLUT_METHOD* pMethod, int argc, char** argv )
 {
 	glutInit( &argc, argv );
+	
 	_JinitGLUT();
+	
+	assert( pMethod != NULL );
+
+	glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE ); // Display Mode
+	glutInitWindowSize( 500, 500 ); // If glutFullScreen wasn't called this is the window size
+	glutCreateWindow( "NeHe's OpenGL Framework" ); // Window Title (argv[0] for current directory as title)
+//	glutFullScreen( );          // Put Into Full Screen
+	glutDisplayFunc( pMethod->JGlutDisplay );  // Matching Earlier Functions To Their Counterparts
+	glutReshapeFunc( pMethod->JGlutReshape );
+	glutKeyboardFunc( pMethod->JGlutKeyboard );
+	glutSpecialFunc( pMethod->JGlutKeyArrowKey );
+	glutIdleFunc( pMethod->JGlutDisplay );
 }
--- trunk/Freetype2 Ex/main.cpp
+++ trunk/Freetype2 Ex/main.cpp
@@ -2,14 +2,95 @@
 #include "freetypeEx.h"
 #include "GLUT/JglutAfx.h"
 
+void display()
+{
+	static GLfloat rtri = 0.0f;
+	static GLfloat rquad = 0.0f;
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
+	glLoadIdentity();									// Reset The Current Modelview Matrix
+	glPushMatrix();
+	glTranslatef(-1.5f,0.0f,-6.0f);						// Move Left 1.5 Units And Into The Screen 6.0
+	glRotatef(rtri,0.0f,1.0f,0.0f);				// Rotate The Triangle On The Y axis
+	glBegin(GL_TRIANGLES);								// Drawing Using Triangles
+	glColor3f(1.0f,0.0f,0.0f);						// Set The Color To Red
+	glVertex3f( 0.0f, 1.0f, 0.0f);					// Top
+	glColor3f(0.0f,1.0f,0.0f);						// Set The Color To Green
+	glVertex3f(-1.0f,-1.0f, 0.0f);					// Bottom Left
+	glColor3f(0.0f,0.0f,1.0f);						// Set The Color To Blue
+	glVertex3f( 1.0f,-1.0f, 0.0f);					// Bottom Right
+	glEnd();											// Finished Drawing The Triangle
+
+	glLoadIdentity();					// Reset The Current Modelview Matrix
+	glTranslatef(1.5f,0.0f,-6.0f);				// Move Right 1.5 Units And Into The Screen 6.0
+	glRotatef(rquad,1.0f,0.0f,0.0f);			// Rotate The Quad On The X axis
+	glColor3f(0.5f,0.5f,1.0f);							// Set The Color To Blue One Time Only
+	glBegin(GL_QUADS);									// Draw A Quad
+	glVertex3f(-1.0f, 1.0f, 0.0f);					// Top Left
+	glVertex3f( 1.0f, 1.0f, 0.0f);					// Top Right
+	glVertex3f( 1.0f,-1.0f, 0.0f);					// Bottom Right
+	glVertex3f(-1.0f,-1.0f, 0.0f);					// Bottom Left
+	glEnd();											// Done Drawing The Quad
+	glPopMatrix();
+	rtri+=0.2f;						// Increase The Rotation Variable For The Triangle ( NEW )
+	rquad-=0.15f;						// Decrease The Rotation Variable For The Quad     ( NEW )
+
+	glutSwapBuffers ( );
+}
+
+void reshape( int w, int h )   // Create The Reshape Function (the viewport)
+{
+	glViewport     ( 0, 0, w, h );
+	glMatrixMode   ( GL_PROJECTION );  // Select The Projection Matrix
+	glLoadIdentity ( );                // Reset The Projection Matrix
+	if ( h==0 )  // Calculate The Aspect Ratio Of The Window
+		gluPerspective ( 80, ( float ) w, 1.0, 5000.0 );
+	else
+		gluPerspective ( 80, ( float ) w / ( float ) h, 1.0, 5000.0 );
+	glMatrixMode   ( GL_MODELVIEW );  // Select The Model View Matrix
+	glLoadIdentity ( );    // Reset The Model View Matrix
+}
+
+void keyboard( unsigned char key, int x, int y )  // Create Keyboard Function
+{
+	switch ( key ) {
+	case 27:        // When Escape Is Pressed...
+		exit ( 0 );   // Exit The Program
+		break;        // Ready For Next Case
+	default:        // Now Wrap It Up
+		break;
+	}
+}
+
+void arrow_keys( int a_keys, int x, int y )  // Create Special Function (required for arrow keys)
+{
+	switch ( a_keys ) {
+	case GLUT_KEY_UP:     // When Up Arrow Is Pressed...
+		glutFullScreen ( ); // Go Into Full Screen Mode
+		break;
+	case GLUT_KEY_DOWN:               // When Down Arrow Is Pressed...
+		glutReshapeWindow ( 500, 500 ); // Go Into A 500 By 500 Window
+		break;
+	default:
+		break;
+	}
+}
 int main()
 {
 	int argc = 1;
 	char* argv[] = { "test", "" };
-	JinitGLUT( NULL, argc, argv );
+
+	JGLUT_METHOD method;
+
+	method.JGlutDisplay = display;
+	method.JGlutReshape = reshape;
+	method.JGlutKeyboard = keyboard;
+	method.JGlutKeyArrowKey = arrow_keys;
+
+	JinitGLUT( &method, argc, argv );
+
 	FT_Library    library;
 	FT_Face       face;
-
 
 	FT_GlyphSlot  slot;
 	FT_Matrix     matrix;                 /* transformation matrix */
@@ -22,8 +103,6 @@ int main()
 	double        angle;
 	int           target_height;
 	int           n, num_chars;
-
-
 
 	filename      = "c:/windows/fonts/malgun.ttf";                           /* first argument     */
 	text          = "Test";                           /* second argument    */
@@ -54,6 +133,12 @@ int main()
 	/* start at (300,200) relative to the upper left corner  */
 	pen.x = 30 * 64;
 	pen.y = ( target_height - 110 ) * 64;
+
+	glutMainLoop();
+	FT_Done_Face    ( face );
+	FT_Done_FreeType( library );
+
+	return 0;
 
 	for ( n = 0; n < num_chars; n++ )
 	{
